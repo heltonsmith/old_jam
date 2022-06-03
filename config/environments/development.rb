b424f93fd387e8312d9db41a8e8826033862505a
Helton Bustos <heltonsmith@hotmail.com>
2022-06-03 17:29:55 -0400

    correos

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,6 +1,20 @@
 require "active_support/core_ext/integer/time"
 
 Rails.application.configure do
+
+  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    :user_name => 'f9ce2524e88aab',
+    :password => '7f6392dc4db9aa',
+    :address => 'smtp.mailtrap.io',
+    :domain => 'smtp.mailtrap.io',
+    :port => '2525',
+    :authentication => :cram_md5
+  }
+
+
   # Settings specified here will take precedence over those in config/application.rb.
 
   # In the development environment your application's code is reloaded any time
